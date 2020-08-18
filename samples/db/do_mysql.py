4e8ac68418a400beb65ba1f9dabf4fde9191cfa2
austinyu <geniusyml@gmail.com>
2020-08-18 18:57:15 +0800

    学了sql的部分

--- samples/db/do_mysql.py
+++ samples/db/do_mysql.py
@@ -9,13 +9,13 @@
 import mysql.connector
 
 # change root password to yours:
-conn = mysql.connector.connect(user='root', password='password', database='test')
+conn = mysql.connector.connect(user='root', password='123456', database='test')
 
 cursor = conn.cursor()
 # 创建user表:
-cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
+# cursor.execute('create table user (id varchar(20) primary key, name varchar(20))')
 # 插入一行记录，注意MySQL的占位符是%s:
-cursor.execute('insert into user (id, name) values (%s, %s)', ('1', 'Michael'))
+cursor.execute('insert into user (id, name) values (%s, %s)', ('8', 'Michael'))
 print('rowcount =', cursor.rowcount)
 # 提交事务:
 conn.commit()
--- samples/db/do_sqlalchemy.py
+++ samples/db/do_sqlalchemy.py
@@ -18,27 +18,28 @@ class User(Base):
     name = Column(String(20))
 
 # 初始化数据库连接:
-engine = create_engine('mysql+mysqlconnector://root:password@localhost:3306/test')
+engine = create_engine('mysql+mysqlconnector://root:123456@localhost:3306/test')
 # 创建DBSession类型:
 DBSession = sessionmaker(bind=engine)
 
-# 创建session对象:
-session = DBSession()
-# 创建新User对象:
-new_user = User(id='5', name='Bob')
-# 添加到session:
-session.add(new_user)
-# 提交即保存到数据库:
-session.commit()
-# 关闭session:
-session.close()
+# # 创建session对象:
+# session = DBSession()
+# # 创建新User对象:
+# new_user = User(id='10', name='Bob')
+# # 添加到session:
+# session.add(new_user)
+# # 提交即保存到数据库:
+# session.commit()
+# # 关闭session:
+# session.close()
 
 # 创建Session:
 session = DBSession()
 # 创建Query查询，filter是where条件，最后调用one()返回唯一行，如果调用all()则返回所有行:
-user = session.query(User).filter(User.id=='5').one()
+# user = session.query(User).filter(User.id=='10').one()
+users = session.query(User).all()
 # 打印类型和对象的name属性:
-print('type:', type(user))
-print('name:', user.name)
+print('type:', type(users))
+print('name:', users)
 # 关闭Session:
 session.close()
